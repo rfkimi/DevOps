17a11bd05e16c58841c6a771864696356a19021f
邵斌 <shaobin@shaobindeMacBook-Pro.local>
2018-09-03 16:58:17 +0800

    update hello_world_producer.py

--- RabbitMQ_Python/hello_world_producer.py
+++ RabbitMQ_Python/hello_world_producer.py
@@ -9,18 +9,22 @@ conn_broker = pika.BlockingConnection(conn_params)
 
 channel = conn_broker.channel()
 
-channel.exchange_declare(exchange="hello-exchange",
-                         exchange_type="direct",
-                         passive=False,
-                         durable=True,
-                         auto_delete=False)
+channel.confirm_delivery()
 # create text message
 msg = sys.argv[1]
 msg_props = pika.BasicProperties()
 msg_props.content_type = "text/plain"
-
+msg_ids = []
 # publish message
-channel.basic_publish(body=msg,
-                      exchange="hello-exchange",
-                      properties=msg_props,
-                      routing_key="hola")
+ack = channel.basic_publish(body=msg,
+                            exchange="hello-exchange",
+                            properties=msg_props,
+                            routing_key="hola")
+if ack:
+    print "put message to rabbitmq successed!"
+else:
+    print "put message to rabbitmq failed"
+
+
+msg_ids.append(len(msg_ids)+1)
+channel.close()
