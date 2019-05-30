66f6f4e9917ac73b35b73a538237a340e26e091e
rfkimi <shaobin@shaobindeMacBook-Pro.local>
2019-05-30 15:27:33 +0800

    add PySnooper test code

---
+++ PySnooper/pysnooper_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import pysnooper
+import random
+
+
+@pysnooper.snoop()
+def number_to_bits(number):
+    if number:
+        bits = []
+        while number:
+            number, remainder = divmod(number, 2)
+            bits.insert(0, remainder)
+        return bits
+    else:
+        return [0]
+
+
+number_to_bits(6)
+
+
+def foo():
+    lst = []
+    for i in range(10):
+        lst.append(random.randrange(1, 1000))
+
+    with pysnooper.snoop():
+        lower = min(lst)
+        upper = max(lst)
+        mid = (lower + upper) / 2
+        print(lower, upper, mid)
+
+foo()
