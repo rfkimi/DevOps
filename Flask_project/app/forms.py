e86089c7a9b4f0c6c3583ceca9daa790667a26b4
rfkimi <shaobin@shaobindeMacBook-Pro.local>
2019-09-08 21:39:27 +0800

    add Flask

--- Flask_project/app/forms.py
+++ Flask_project/app/forms.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 
-class LoginFrom(Form):
+class LoginFrom(FlaskForm):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
--- Flask_project/app/views.py
+++ Flask_project/app/views.py
@@ -2,7 +2,6 @@
 # encoding: utf-8
 from app import app
 from flask import render_template, flash, redirect
-from .forms import LoginFrom
 
 
 @app.route('/')
@@ -27,7 +26,14 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginFrom
+    form = LoginFrom()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="'
+              + form.openid.data +
+              ', remember_me=' +
+              str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
-                           form=form)
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
